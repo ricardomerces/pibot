28106b78d3c698bfe2d5182a48d631e1f3e45ed7
Ricardo Merces <ricardo.merces@gmail.com>
2016-11-30 12:27:34 -0200

    Update distance.py

--- distance.py
+++ distance.py
@@ -22,19 +22,19 @@ def initialization():
     GPIO.setup(TRIG,GPIO.OUT)
 
 def distance():
-    GPIO.output(TRIG,GPIO.HIGH)
-    time.sleep (0.000010)
-    GPIO.output(TRIG,GPIO.LOW)
-    while GPIO.input(ECHO) == 0:
-        pulse_start = time.time()
-    while GPIO.input(ECHO) == 1:
-        pulse_end = time.time()
-    pulse = pulse_end - pulse_start
-    distance = pulse/2 * 34300  # 343m/s Speed of Sound
-    distance = round(distance,0)
-    print (distance, 'cm   ' , end="\r")
+    while True:
+        time.sleep(2)
+        GPIO.output(TRIG,GPIO.HIGH)
+        time.sleep (0.000010)
+        GPIO.output(TRIG,GPIO.LOW)
+        while GPIO.input(ECHO) == 0:
+            pulse_start = time.time()
+        while GPIO.input(ECHO) == 1:
+            pulse_end = time.time()
+        pulse = pulse_end - pulse_start
+        distance = pulse/2 * 34300  # 343m/s Speed of Sound
+        distance = round(distance,0)
+        print (distance, 'cm   ' , end="\r")
 
 initialization()
-while True:
-    time.sleep(2)
-    distance()
+distance()
